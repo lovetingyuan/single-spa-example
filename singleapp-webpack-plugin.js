950f2663c19afb4d76d136bbf933df9c960737ef
lovetingyuan <1932294867@qq.com>
2020-03-20 17:18:38 +0800

    remove useless code

--- singleapp-webpack-plugin.js
+++ singleapp-webpack-plugin.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const fs = require('fs')
 const crypto = require('crypto')
 const webpack = require('webpack')
 const ManifestPlugin = require('webpack-manifest-plugin')
@@ -11,31 +10,26 @@ module.exports = class SingleAppPlugin {
     const { singleapp, name } = options.package
     const appName = (singleapp.name || name).replace(/ /g, '-')
     const appRoute = singleapp.route || '/'
-    let port = singleapp.port || parseInt(
+    let port = +process.env.PORT || +singleapp.port || parseInt(
       crypto.createHash('md5').update(appName).digest('hex').substr(0, 10), 16
     ) % 10000
-    if (port < 1000) {
-      port = port * 10
-    }
+    port = port * (10 ** (4 - String(port).length))
+
     this.port = port
     this.appName = appName
     this.appRoute = appRoute
   }
   apply(compiler) {
     if (this.options.disable) return
+
     const port = this.port
     const appName = this.appName
     const appRoute = this.appRoute
     const devHost = 'http://localhost:' + port
-    const manifestDir = path.resolve(__dirname, 'manifest')
-    if (!fs.existsSync(manifestDir)) {
-      fs.mkdirSync(manifestDir)
-    }
-    const manifestFilePath = path.resolve(manifestDir, appName + '.json')
-    fs.writeFileSync(manifestFilePath, '{}')
+
     const manifestPlugin = new ManifestPlugin({
       writeToFileEmit: true,
-      fileName: manifestFilePath,
+      fileName: path.resolve(__dirname, 'manifest', appName + '.json'),
       generate(seed, files, entrypoints) {
         const assets = files.reduce((manifest, {name, path, isInitial}) => {
           return {...manifest, [name]: path}
